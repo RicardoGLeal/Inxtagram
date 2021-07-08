3aa23deec5a701881e8811d0ea5d65cdd52c89ab
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2021-07-07 21:09:54 -0500

    Moved PostAdapter into an Adapters package

--- app/src/main/java/com/example/inxtagram/Adapters/PostsAdapter.java
+++ app/src/main/java/com/example/inxtagram/Adapters/PostsAdapter.java
@@ -1,4 +1,4 @@
-package com.example.inxtagram;
+package com.example.inxtagram.Adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -6,11 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.inxtagram.Post;
+import com.example.inxtagram.R;
 import com.parse.ParseFile;
 
 import java.util.List;
@@ -51,16 +56,20 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
         notifyDataSetChanged();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private TextView tvUsername;
         private ImageView ivImage;
         private TextView tvDescription;
+        private ImageView ivProfilePicture;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvUsername = itemView.findViewById(R.id.tvUsername);
             ivImage = itemView.findViewById(R.id.ivImage);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+            ivProfilePicture = itemView.findViewById(R.id.ivProfileImage);
+
+            itemView.setOnClickListener(this);
         }
 
         public void bind(Post post) {
@@ -68,10 +77,37 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
             tvDescription.setText(post.getDescription());
             tvUsername.setText(post.getUser().getUsername());
             ParseFile image = post.getImage();
+            ParseFile profilePicture = post.getUser().getParseFile("profilePicture");
+
+            RequestOptions circleProp = new RequestOptions();
+            circleProp = circleProp.transform(new CircleCrop());
+
+            if (profilePicture != null) {
+                Glide.with(context)
+                        .load(profilePicture.getUrl())
+                        .placeholder(R.drawable.profile_image_empty)
+                        .apply(circleProp)
+                        .into(ivProfilePicture);
+            }
+            else {
+                Glide.with(context)
+                        .load(R.drawable.profile_image_empty)
+                        .placeholder(R.drawable.profile_image_empty)
+                        .apply(circleProp)
+                        .into(ivProfilePicture);
+            }
 
             //condition to check if there is an image attached
             if (image != null) {
                 Glide.with(context).load(image.getUrl()).into(ivImage);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                Toast.makeText(context, "asfd", Toast.LENGTH_SHORT).show();
             }
         }
     }
