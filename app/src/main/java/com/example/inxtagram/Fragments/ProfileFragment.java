657af136307f3ef433362b289a16c9d38f1704b3
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2021-07-07 21:09:22 -0500

    Now the ProfileFragment shows also a default profile picture.

--- app/src/main/java/com/example/inxtagram/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/inxtagram/Fragments/ProfileFragment.java
@@ -1,13 +1,30 @@
 package com.example.inxtagram.Fragments;
 
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.inxtagram.Post;
+import com.example.inxtagram.Adapters.PostsAdapter;
+import com.example.inxtagram.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -15,6 +32,70 @@ Since the content displayed in the home fragment is almost the same as that disp
 ProfileFragment inherits from PostsFragment, which is more efficient than repeating code.
  */
 public class ProfileFragment extends PostsFragment {
+
+    public static final String TAG = "ProfileFragment";
+    private RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
+    private SwipeRefreshLayout swipeContainer;
+    private ImageView ivProfile;
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ivProfile = view.findViewById(R.id.ivProfileImage);
+
+        RequestOptions circleProp = new RequestOptions();
+        circleProp = circleProp.transform(new CircleCrop());
+        Glide.with(getContext())
+                .load("https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51405259-stock-illustration-male-avatar-profile-picture-use.jpg")
+                .apply(circleProp)
+                .into(ivProfile);
+
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        /**
+         * OnClickListener implemented when the user pulls to refresh.
+         */
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                adapter.clear();
+                allPosts.clear();
+                queryPosts();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        adapter = new PostsAdapter(getContext(), allPosts);
+
+        //Steps to use the recycler view:
+        //0. create layout for one row in the list
+        //1. create the adapter
+        //2. create the data source
+        //3. set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+        //4. set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+    }
+
 
     @Override
     protected void queryPosts() {
