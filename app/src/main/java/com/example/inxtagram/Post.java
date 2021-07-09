37b3d640fa41009446a4fa61443591f46315111b
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2021-07-08 20:01:37 -0500

    Added functions to Post Model to set and get likes

--- app/src/main/java/com/example/inxtagram/Post.java
+++ app/src/main/java/com/example/inxtagram/Post.java
@@ -5,12 +5,17 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ParseClassName("Post")
 public class Post extends ParseObject {
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
     public static final String KEY_CREATED_KEY = "createdAt";
+    public static final String KEY_LIKED_BY = "likedBy";
+
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
     }
@@ -33,5 +38,39 @@ public class Post extends ParseObject {
 
     public void setUser(ParseUser user) {
         put(KEY_USER, user);
+    }
+
+    public int getLikesNumber() {
+        List<String> list = getList(KEY_LIKED_BY);
+        if (list != null)
+            return list.size();
+        else
+            return 0;
+    }
+
+    public void addLike(String id) {
+        List<String> list = getList(KEY_LIKED_BY);
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+        list.add(id);
+        setLikedBy(list);
+    }
+
+    public boolean isLiked() {
+        List<String> list = getList(KEY_LIKED_BY);
+        return list!=null && list.contains(ParseUser.getCurrentUser().getObjectId());
+    }
+
+    private void setLikedBy(List<String> list) {
+        put(KEY_LIKED_BY, list);
+    }
+
+    public void removeLike(String id){
+        List<String> list = getList(KEY_LIKED_BY);
+        if(list==null)
+            list = new ArrayList<>();
+        list.remove(id);
+        setLikedBy(list);
     }
 }
